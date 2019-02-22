88eaf5c5c8810a48a27db5413025534f3064d9cc
SimonL <lonelywolf4254@gmail.com>
2019-02-22 17:31:12 -0500

    animation marche pas

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+
+    console.log("oui animation");
+  
+   /* let element = document.getElementsByClassName('entry-title');
+    element.classList.add("mystyle");*/
+
+    console.log(document.getElementsByClassName('entry-title'));
+    //.classList.add("barre");
+
+})
