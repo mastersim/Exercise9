98797955592b05038ec09b0026967c4bc9a259ec
SimonL <lonelywolf4254@gmail.com>
2019-03-01 18:09:01 -0500

    couleur background marchent

--- template-parts/content/content-titre-cours.php
+++ template-parts/content/content-titre-cours.php
@@ -11,6 +11,30 @@
 
 ?>
 <?php
-	/*echo get_the_title();*/
-	the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+
+	if(substr(get_the_title(), 4, 1) == 1){
+		the_title( sprintf( '<h2 class="entry-title blue"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+	}	
+
+	if(substr(get_the_title(), 4, 1) == 2){
+		the_title( sprintf( '<h2 class="entry-title green"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+	}	
+
+	if(substr(get_the_title(), 4, 1) == 3){
+		the_title( sprintf( '<h2 class="entry-title red"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+	}
+
+	if(substr(get_the_title(), 4, 1) == 4){
+		the_title( sprintf( '<h2 class="entry-title yellow"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+	}
+
+	if(substr(get_the_title(), 4, 1) == 5){
+		the_title( sprintf( '<h2 class="entry-title purple"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+	}
+
+	if(substr(get_the_title(), 4, 1) == 6){
+		the_title( sprintf( '<h2 class="entry-title orange"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+	}
+
+	/*the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );*/
 ?>
